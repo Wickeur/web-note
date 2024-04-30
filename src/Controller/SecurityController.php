eb817507c819c4bbcde5f088d508fecc4f30b7a8
LEULIET Quentin <leuliet.quentin.online@gmail.com>
2024-04-30 15:11:45 +0200

    System d'authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private $passwordHasher;
+    private $entityManager;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -28,5 +44,48 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request): Response
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            // Check if the email is already registered
+            $existingUser = $this->entityManager->getRepository(User::class)->findOneBy([
+                'email' => $formData->getEmail()
+            ]);
+
+            if ($existingUser) {
+                // Redirect back with an error message
+                $this->addFlash('danger', 'Email is already registered.');
+                return $this->redirectToRoute('app_register');
+            }
+
+            // Hash the password
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $formData->getPassword());
+            $user->setPassword($hashedPassword);
+
+            // Persist the new user
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            // Redirect to login page after successful registration
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
